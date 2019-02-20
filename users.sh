4128b6997cfdfe125c79f4a732574fa6e7691c35
Jordan Johnson <jordanjohnson56@users.noreply.github.com>
2019-02-20 12:59:11 -0600

    Update users.sh

--- users.sh
+++ users.sh
@@ -8,6 +8,7 @@ _p="/etc/passwd" ## get mini UID limit ##
 l=$(grep "^UID_MIN" $_l) ## get max UID limit ##
 l1=$(grep "^UID_MAX" $_l) ## use awk to print if UID >= $MIN and UID <= $MAX and shell is not /sbin/nologin ##
 echo "----------[ Normal User Accounts ]---------------"
-awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max && $7 != "/sbin/nologin" ) print $0 }' "$_p" echo ""
+awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max && $7 != "/sbin/nologin" ) print $0 }' "$_p"
+echo ""
 echo "----------[ System User Accounts ]---------------"
 awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( !($3 >= min && $3 <= max && $7 != "/sbin/nologin")) print $0 }' "$_p"
