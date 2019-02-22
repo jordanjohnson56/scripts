2d367887e75659861e6a66ba37b83eb6eb922e35
Jordan Johnson <jordanjohnson56@users.noreply.github.com>
2019-02-22 15:25:59 -0600

    Update get_rpm_for.sh

--- get_rpm_for.sh
+++ get_rpm_for.sh
@@ -2,4 +2,3 @@ wget "https://download.splunk.com/products/universalforwarder/releases/7.2.4/lin
 rpm -U splunkforwarder-7.2.4-8a94541dcfac-linux-2.6-x86_64.rpm
 /opt/splunkforwarder/bin/splunk enable boot-start --accept-license --answer-yes
 /opt/splunkforwarder/bin/splunk set deploy-poll 172.20.241.20:8089
-service splunk start
