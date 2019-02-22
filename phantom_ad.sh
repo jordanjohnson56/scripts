87776a412bd3123cc4e8ef7b92691f856d1f0893
Jordan Johnson <jordanjohnson56@users.noreply.github.com>
2019-02-22 12:30:45 -0600

    Update phantom_ad.sh

--- phantom_ad.sh
+++ phantom_ad.sh
@@ -1,1 +1,2 @@
 yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python -y
+realm join --user=Administrator ccdc.com
