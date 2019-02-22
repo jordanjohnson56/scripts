28b6454f16d92d24ae4926a3cfece8f8a11038b2
Jordan Johnson <jordanjohnson56@users.noreply.github.com>
2019-02-22 15:41:01 -0600

    Update get_deb_for.sh

--- get_deb_for.sh
+++ get_deb_for.sh
@@ -1,3 +1,4 @@
 wget "https://download.splunk.com/products/universalforwarder/releases/7.2.4/linux/splunkforwarder-7.2.4-8a94541dcfac-linux-2.6-amd64.deb"
+dpkg -i splunkforwarder-7.2.4-8a94541dcfac-linux-2.6-amd64.deb
 /opt/splunkforwarder/bin/splunk enable boot-start --accept-license --answer-yes
 /opt/splunkforwarder/bin/splunk set deploy-poll 172.20.241.20:8089
