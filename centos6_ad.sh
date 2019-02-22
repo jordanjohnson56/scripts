10932d7f186379f7509dc46ec8b0a0a3c2ff3132
Jordan Johnson <jordanjohnson56@users.noreply.github.com>
2019-02-22 16:54:42 -0600

    Update centos6_ad.sh

--- centos6_ad.sh
+++ centos6_ad.sh
@@ -1,1 +1,7 @@
-yum --enablerepo=epel -y install adcli sssd authconfig pam_krb5 samba4-common
+yum -y install adcli sssd authconfig pam_krb5 samba4-common
+authconfig --enablekrb5 --krb5kdc=fd3s.srv.world --krb5adminserver=fd3s.srv.world --krb5realm=FD3S.SRV.WORLD --enablesssd --enablesssdauth --update
+adcli join CCDC.COM
+cp ./sssd.conf /etc/sssd/sssd.conf
+chmod 600 /etc/sssd/sssd.conf
+echo "session     optional      pam_mkhomedir.so skel=/etc/skel umask=077" >> /etc/pam.d/system-auth
+/etc/rc.d/init.d/sssd start
