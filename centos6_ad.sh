cc3f4f7462b35dedc33e33f533ffb9aa05981ca0
Jordan Johnson <jordanjohnson56@users.noreply.github.com>
2019-02-22 21:02:09 -0600

    Update centos6_ad.sh

--- centos6_ad.sh
+++ centos6_ad.sh
@@ -1,6 +1,9 @@
+echo "Domain (UPPERCASE): "
+read domain
+
 yum -y install adcli sssd authconfig pam_krb5 samba4-common
-authconfig --enablekrb5 --krb5kdc=fd3s.srv.world --krb5adminserver=fd3s.srv.world --krb5realm=FD3S.SRV.WORLD --enablesssd --enablesssdauth --update
-adcli join CCDC.COM
+authconfig --enablekrb5 --krb5kdc=$domain --krb5adminserver=$domain --krb5realm=$domain --enablesssd --enablesssdauth --update
+adcli join $domain
 cp ./sssd.conf /etc/sssd/sssd.conf
 chmod 600 /etc/sssd/sssd.conf
 echo "session     optional      pam_mkhomedir.so skel=/etc/skel umask=077" >> /etc/pam.d/system-auth
