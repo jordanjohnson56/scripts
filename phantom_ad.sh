f83885a9e5473b82aaef4c31576d8fc1c061a92c
Jordan Johnson <jordanjohnson56@users.noreply.github.com>
2019-02-22 21:05:45 -0600

    Update phantom_ad.sh

--- phantom_ad.sh
+++ phantom_ad.sh
@@ -1,3 +1,9 @@
+echo "Domain (UPPERCASE): "
+read domain
+
+echo "User: "
+read user
+
 yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python -y
-realm join --user=Administrator ccdc.com
-kinit jjohnson
+realm join --user=Administrator $domain
+kinit $user
